fa80faefb366535bbbd5df9b97d80dece9e237e8
Adolph C <adolphc@outlook.com>
2015-09-01 22:04:33 -0400

    Cling works.
    The video can now cling. It is much smoother than the previous version
    of cling and better animated.

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,5 +1,8 @@
 var __Cling__ = (function() {
 	var __DEBUG_MODE__ = true;
+	var SCROLL_INTERVAL_MS = 60;
+	var ANIMATE_IN = "flipInX";
+	var ANIMATE_OUT = "flipOutX";
 
 	function log() {
 		if(__DEBUG_MODE__ && arguments.length > 0) {
@@ -21,6 +24,12 @@ var __Cling__ = (function() {
 		this.isFlashVideo = false;
 		this.oldVideoParent = null;
 		this.videoElement = null;
+		this.scrollChangeInterval = null;
+		this.lastScrollPosition = -1;
+		this.clinging = false;
+		this.videoState = {
+			"paused": false
+		};
 	}
 
 	/**
@@ -28,6 +37,7 @@ var __Cling__ = (function() {
 	 */
 	Cling.prototype.init = function() {
 		log("Initializing cling...");
+		this.bindScrollListener(); // #todo NEXT
 		this.bindURLChange();
 	}
 
@@ -51,7 +61,31 @@ var __Cling__ = (function() {
 	 */
 	Cling.prototype.onURLChange = function() {
 		log("URL changed: %s", this.currentURL);
+		this.uncling();
+		// this.takeVideo();
+	}
+
+	Cling.prototype.cling = function() {
+		if(this.clinging) {
+			return;
+		}
+		this.clinging = true;
+		this.ensureClingWindow();
+		this.clingWindow.classList.remove("ext-cling-window-hidden");
+		this.clingWindow.classList.add("animated");
+		this.clingWindow.classList.remove(ANIMATE_OUT);
+		this.clingWindow.classList.add(ANIMATE_IN);
 		this.takeVideo();
+	}
+
+	Cling.prototype.uncling = function() {
+		if(this.clinging && this.clingWindow) {
+			this.clingWindow.classList.add("animated");
+			this.clingWindow.classList.remove(ANIMATE_IN);
+			this.clingWindow.classList.add(ANIMATE_OUT);
+			this.releaseVideo();
+		}
+		this.clinging = false;
 	}
 
 	/**
@@ -82,19 +116,47 @@ var __Cling__ = (function() {
 		}
 	}
 
+	Cling.prototype.findVideoElement = function() {
+		return document.querySelector("video.video-stream.html5-main-video");
+	}
+
+	// #todo create HTML5 and flash version?
+	Cling.prototype.commitVideoState = function() {
+		if(this.videoElement) {
+			this.videoState.paused = this.videoElement.paused;
+		}
+	}
+
+	// #todo create HTML5 and flash version?
+	Cling.prototype.restoreVideoState = function() {
+		if(this.videoElement) {
+			if(this.videoState.paused) {
+				this.videoElement.pause();
+			} else {
+				this.videoElement.play();
+			}
+		}
+	}
+
 	Cling.prototype.takeHtml5Video = function() {
 		log("Attempting to take video...");
+		this.commitVideoState();
 		this.ensureClingWindow();
-		this.videoElement = document.querySelector("video.video-stream.html5-main-video");
+		this.videoElement = this.findVideoElement();
 		this.oldVideoParent = this.videoElement.parentElement;
 		this.clingWindow.appendChild(this.videoElement);
+		var _self = this;
+		_self.restoreVideoState();
 	}
 
 	Cling.prototype.releaseHtml5Video = function() {
 		if(this.videoElement) {
+			this.commitVideoState();
 			this.oldVideoParent.appendChild(this.videoElement);
-			this.videoElement = null;
 			this.oldVideoParent = null;
+			var _self = this;
+			_self.restoreVideoState();
+			this.videoElement = null;
 		}
 	}
 
@@ -107,6 +169,7 @@ var __Cling__ = (function() {
 		}
 		this.clingWindow = document.createElement("div");
 		this.clingWindow.classList.add("ext-cling-window");
+		this.clingWindow.classList.add("ext-cling-window-hidden");
 		document.body.appendChild(this.clingWindow);
 
 		var ytHeader = document.getElementById("yt-masthead-container");
@@ -123,6 +186,39 @@ var __Cling__ = (function() {
 	Cling.prototype.ensureClingWindow = function() {
 		if(!this.clingWindow) {
 			this.createClingWindow();
+		}
+	}
+
+	Cling.prototype.bindScrollListener = function() {
+		if(this.scrollChangeInterval) {
+			clearInterval(this.scrollChangeInterval);
+			this.scrollChangeInterval = null;
+		}
+		var _self = this;
+		this.scrollChangeInterval = setInterval(function() {
+			if(this.lastScrollPosition != window.scrollY) {
+				this.lastScrollPosition = window.scrollY;
+				_self.onScroll();
+			}
+		}, SCROLL_INTERVAL_MS);
+	}
+
+	Cling.prototype.unbindScrollListener = function() {
+		if(this.scrollChangeInterval) {
+			clearInterval(this.scrollChangeInterval);
+			this.scrollChangeInterval = null;
+		}
+	}
+
+	Cling.prototype.onScroll = function() {
+		var video = this.findVideoElement();
+		var bottom = video.offsetHeight + video.offsetTop;
+		if(window.scrollY > bottom) {
+			log("CLING");
+			this.cling();
+		} else {
+			log("UNCLING");
+			this.uncling();
 		}
 	}
 
